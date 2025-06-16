609bea4b32ee66ca9c2d9eabc654d12647ea0e4f
RileyWren <zainab.42847@gmail.com>
2025-06-16 08:57:13 +0600

    updated

--- app/components/TopProduct.jsx
+++ app/components/TopProduct.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 const TopProduct = () => {
   return (
-    <div></div>
+    <div>
+      <div>
+            <Image
+      src="/profile.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+
+      </div>
+    </div>
   )
 }
 
